9a0b98dc8b264e1105a033c84ee9d1f3e40e414b
kerestell@gmail.com <kerestell@gmail.com>
2024-09-02 20:49:51 +0100

    (Do Not Compile) Added methods to invoke later on!

--- Mod.cs
+++ Mod.cs
@@ -1,32 +1,26 @@
 using BepInEx;
 using Photon.Pun;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 namespace TheGorillaWatch
 {
-    [BepInPlugin("com.ArtificialGorillas.gorillatag.GorillaWatch", "GorillaWatch", "1.3.1")]
-    public class Mod : BaseUnityPlugin
+    [BepInPlugin("ArtificialGorillas.GorillaWatch", "GorillaWatch", "1.4.0")]
+    public class Main : BaseUnityPlugin
     {
+        Vector3 oringalGravity;
+
         public static int counter;
 
         public static float PageCoolDown;
 
-        public static int layer = 29, layerMask = 1 << layer;
-
-        private LayerMask baseMask;
-
-        float bounce;
-
-        private bool toggleWatch = true;
-
-        PhysicMaterialCombine PMCombine;
-
         int modCount;
 
-        public static Dictionary<string, bool> Mods;
+        public static Dictionary<Mod, bool> Mods;
 
         public static GameObject leftplat = null;
 
@@ -43,47 +37,47 @@ namespace TheGorillaWatch
 
         public static GameObject Swim = null;
 
-        public static Vector3[] lastLeft = new Vector3[] { Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero };
+        public static Vector3[] lastLeft = new Vector3[] {};
 
-        public static Vector3[] lastRight = new Vector3[] { Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero };
+        public static Vector3[] lastRight = new Vector3[] {};
 
         Vector3 AddForceStuff = new Vector3(0f, 40f, 0f);
         bool doneDeletion;
 
         void Start()
         {
-            Mods = new Dictionary<string, bool>()
+            Mods = new Dictionary<Mod, bool>()
             {
-                { "GorillaWatch!\nMade by:\nARTIFICIALGORILLAS", false },
-                { "PlatformGuy", false },
-                { "FrozoneGuy", false },
-                { "DrawingGuy", false },
-                { "NoClip", false },
-                { "HoverMonke", false },
-                { "VelocityFly", false },
-                { "HighGravity", false },
-                { "LowGravity", false },
-                { "NoGravity", false },
-                { "BigMonkers", false },
-                { "SmallMonkers", false },
-                { "MonkePunch", false },
-                { "MonkeBoing", false },
-                { "AirSwim", false },
+                { new Mod("Gorilla Watch!", "Triggers To Switch Pages\nA To Toggle."), false },
+                { new Mod("Platforms","Press grip to use them!"), false },
+                { new Mod("Frozone", "Press grip to spawn slip plats!"), false },
+                { new Mod("Drawing", "Press grip to draw!"), false },
+                { new Mod("Noclip", "Disables every collider!\n(Plats suggested)"), false },
+                { new Mod("Flight", "Press X to fly!"), false },
+                { new Mod("Iron Monk", "Press GRIP to fly like iron man!"), false },
+                { new Mod("High Gravity", "Makes you have higher gravity!"), false },
+                { new Mod("Low Gravity", "Makes you have lower gravity!"), false },
+                { new Mod("No Gravity", "Makes you have no gravity!"), false },
+                { new Mod("Big Monk", "Makes your monke bigger!"), false },
+                { new Mod("Small Monk", "Makes your monke smaller!"), false },
+                { new Mod("Monke Punch", "Lets you be punched around by other players!"), false },
+                { new Mod("Monke Boing", "Makes the ground bouncy!"), false },
+                { new Mod("Air Swim", "Swim everywhere!"), false },
             };
             modCount = Mods.Count;
+            oringalGravity = Physics.gravity;
         }
         GorillaHuntComputer huntComputer;
         Text huntText;
         void Update()
         {
-            if (inModded())
+            if (InModded())
             {
                 if (!doneDeletion)
                 {
                     huntComputer = GorillaTagger.Instance.offlineVRRig.huntComputer.GetComponent<GorillaHuntComputer>();
                     huntText = huntComputer.text;
                     huntComputer.enabled = false;
-                    huntComputer.gameObject.SetActive(true);
                     GameObject.Destroy(huntComputer.material);
                     GameObject.Destroy(huntComputer.badge);
                     GameObject.Destroy(huntComputer.leftHand);
@@ -93,14 +87,15 @@ namespace TheGorillaWatch
                     Debug.Log("GorillaWatch Has Loaded Successfully");
                     doneDeletion = true;
                 }
+                huntComputer.gameObject.SetActive(true);
 
-                if (ControllerInputPoller.instance.rightControllerIndexFloat >= .5f && Time.time > PageCoolDown + 0.5)
+                if ((ControllerInputPoller.instance.rightControllerIndexFloat >= .5f || Keyboard.current.rightArrowKey.wasPressedThisFrame) && Time.time > PageCoolDown + 0.5)
                 {
                     PageCoolDown = Time.time;
                     counter++;
                     GorillaTagger.Instance.offlineVRRig.PlayHandTapLocal(67, true, 1f);
                 }
-                if (ControllerInputPoller.instance.leftControllerIndexFloat >= .5f && Time.time > PageCoolDown + 0.5)
+                if (ControllerInputPoller.instance.leftControllerIndexFloat >= .5f || Keyboard.current.leftArrowKey.wasPressedThisFrame && Time.time > PageCoolDown + 0.5)
                 {
                     PageCoolDown = Time.time;
                     counter--;
@@ -113,14 +108,14 @@ namespace TheGorillaWatch
 
                 if (counter != 0)
                 {
-                    huntText.text = $"{Mods.ElementAt(counter).Key}--{Mods.ElementAt(counter).Value.ToString()}";
-                    if (ControllerInputPoller.instance.rightControllerPrimaryButton && Time.time > PageCoolDown + .5)
+                    huntText.text = $"{Mods.ElementAt(counter).Key.Name}: {Mods.ElementAt(counter).Value}.\n{Mods.ElementAt(counter).Key.Desc}".ToUpper();
+                    if (ControllerInputPoller.instance.rightControllerPrimaryButton || Keyboard.current.enterKey.wasPressedThisFrame && Time.time > PageCoolDown + .5)
                     {
                         PageCoolDown = Time.time;
                         Mods[Mods.ElementAt(counter).Key] = !Mods.ElementAt(counter).Value;
                         GorillaTagger.Instance.offlineVRRig.PlayHandTapLocal(69, true, 1f);
                     }
-                }else huntText.text = Mods.ElementAt(counter).Key;
+                }else huntText.text = Mods.ElementAt(counter).Key.Name + "\n" + Mods.ElementAt(counter).Key.Desc;
 
                 #region Platform
                 if (Mods[Mods.ElementAt(1).Key])
@@ -182,13 +177,12 @@ namespace TheGorillaWatch
                 #region Frozone
                 if (Mods[Mods.ElementAt(2).Key])
                 {
-                    Vector3 leftOffset = new Vector3(0f, -0.06f, 0f);
-                    Vector3 rightOffset = new Vector3(0f, -0.06f, 0f);
+                    Vector3 offset = new Vector3(0f, -0.06f, 0f);
 
                     if (ControllerInputPoller.instance.leftGrab)
                     {
                         Frozone = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                        Frozone.transform.position = GorillaLocomotion.Player.Instance.leftControllerTransform.position + leftOffset;
+                        Frozone.transform.position = GorillaLocomotion.Player.Instance.leftControllerTransform.position + offset;
                         Frozone.transform.rotation = GorillaLocomotion.Player.Instance.leftControllerTransform.rotation;
                         Frozone.transform.localScale = new Vector3(0.02f, 0.270f, 0.353f);
                         Frozone.GetComponent<Renderer>().material.shader = Shader.Find("GorillaTag/UberShader");
@@ -202,7 +196,7 @@ namespace TheGorillaWatch
                     if (ControllerInputPoller.instance.rightGrab)
                     {
                         FrozoneR = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                        FrozoneR.transform.position = GorillaLocomotion.Player.Instance.rightControllerTransform.position + leftOffset;
+                        FrozoneR.transform.position = GorillaLocomotion.Player.Instance.rightControllerTransform.position + offset;
                         FrozoneR.transform.rotation = GorillaLocomotion.Player.Instance.rightControllerTransform.rotation;
                         FrozoneR.transform.localScale = new Vector3(0.02f, 0.270f, 0.353f);
                         FrozoneR.GetComponent<Renderer>().material.shader = Shader.Find("GorillaTag/UberShader");
@@ -268,7 +262,7 @@ namespace TheGorillaWatch
                 }
                 #endregion
 
-                #region HoverMonke
+                #region Flight
                 if (Mods[Mods.ElementAt(5).Key])
                 {
                     if (ControllerInputPoller.instance.leftControllerPrimaryButton)
@@ -297,22 +291,24 @@ namespace TheGorillaWatch
                 #region High Gravity
                 if (Mods[Mods.ElementAt(7).Key])
                 {
-                    GorillaLocomotion.Player.Instance.bodyCollider.attachedRigidbody.AddForce(Vector3.down * (Time.deltaTime * (7.77f / Time.deltaTime)), ForceMode.Acceleration);
+                    Physics.gravity = new Vector3(0, 9.8f, 0);
                 }
+                else if (!Mods[Mods.ElementAt(8).Key] && !Mods[Mods.ElementAt(9).Key]) Physics.gravity = oringalGravity;
                 #endregion
 
                 #region Low Gravity
                 if (Mods[Mods.ElementAt(8).Key])
                 {
-                    GorillaLocomotion.Player.Instance.bodyCollider.attachedRigidbody.AddForce(Vector3.up * 10f, ForceMode.Acceleration);
+                    Physics.gravity = new Vector3(0, -5, 0);
                 }
+                else if (!Mods[Mods.ElementAt(9).Key] && !Mods[Mods.ElementAt(7).Key]) Physics.gravity = oringalGravity;
                 #endregion
 
                 #region No Gravity
                 if (Mods[Mods.ElementAt(9).Key])
                 {
-                    GorillaLocomotion.Player.Instance.bodyCollider.attachedRigidbody.AddForce(Vector3.up * (Time.deltaTime * (6.66f / Time.deltaTime)), ForceMode.Acceleration);
-                }
+                    Physics.gravity = Vector3.zero;
+                } else if (!Mods[Mods.ElementAt(8).Key] &&! Mods[Mods.ElementAt(7).Key]) Physics.gravity = oringalGravity;
                 #endregion
 
                 #region BigMonk
@@ -365,15 +361,11 @@ namespace TheGorillaWatch
                 #region BouncyMonk
                 if (Mods[Mods.ElementAt(13).Key])
                 {
-                    bounce = GorillaLocomotion.Player.Instance.bodyCollider.material.bounciness;
-                    PMCombine = GorillaLocomotion.Player.Instance.bodyCollider.material.bounceCombine;
                     GorillaLocomotion.Player.Instance.bodyCollider.material.bounceCombine = PhysicMaterialCombine.Maximum;
                     GorillaLocomotion.Player.Instance.bodyCollider.material.bounciness = 1.0f;
                 }
                 if (!Mods[Mods.ElementAt(13).Key])
                 {
-                    bounce = GorillaLocomotion.Player.Instance.bodyCollider.material.bounciness;
-                    PMCombine = GorillaLocomotion.Player.Instance.bodyCollider.material.bounceCombine;
                     GorillaLocomotion.Player.Instance.bodyCollider.material.bounceCombine = PhysicMaterialCombine.Maximum;
                     GorillaLocomotion.Player.Instance.bodyCollider.material.bounciness = 0f;
                 }
@@ -399,16 +391,34 @@ namespace TheGorillaWatch
                     UnityEngine.Object.Destroy(Swim);
                 }
                 #endregion
-            }
+            }else huntComputer.gameObject.SetActive(true);
         }
 
-        bool inModded()
+        bool InModded()
         {
             if (PhotonNetwork.InRoom)
             {
                 return PhotonNetwork.CurrentRoom.CustomProperties["gameMode"].ToString().Contains("MODDED");
             }
             return false;
+        }
+    }
+
+    public class Mod
+    {
+        public string Name { get; set; }
+        public string Desc { get; set; }
+        public Action OnEnabledMethod { get; set; }
+        public Action StayEnabledMethod { get; set; }
+        public Action OnDisabledMethod { get; set; }
+
+        public Mod(string name, string desc, Action onEnabled, Action stayEnabled, Action onDisabled)
+        {
+            Name = name;
+            Desc = desc;
+            OnEnabledMethod = onEnabled;
+            StayEnabledMethod = stayEnabled;
+            OnDisabledMethod = onDisabled;
         }
     }
 }
