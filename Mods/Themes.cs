471f89694f2d7ceda60be0a8ff918cf779d897bb
evelyn <kerestell@gmail.com>
2024-09-04 13:54:06 +0100

    CoC Board

---
+++ Mods/Themes.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace EveWatch.Mods
+{
+    public class Themes
+    {
+        #region Change Theme
+        static int currentThemeIndex;
+        static List< Color> currentThemes =  new List<Color>
+        {
+            new Color(1, 1, 1, 1), //Normal
+            new Color(1, 0, 0, 1), //Red, Black Background
+            new Color(1, 0, 1, 1), //Purple, Black Background
+            new Color(0, 0, 1, 1), //Blue, Black Background
+        };
+        public static void SwitchTheme()
+        {
+            currentThemeIndex++;
+            if (currentThemeIndex == currentThemes.Count) currentThemeIndex = 0;
+            Main.huntComputer.transform.GetChild(1).GetComponent<Renderer>().sharedMaterial.color = currentThemes[currentThemeIndex];
+        }
+        #endregion
+    }
+}
--- Plugin.cs
+++ Plugin.cs
@@ -7,6 +7,8 @@ using EveWatch.Mods;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
+using System.Net;
+using TMPro;
 
 namespace EveWatch
 {
@@ -23,17 +25,18 @@ namespace EveWatch
         {
             Mods = new Dictionary<Mod, bool>()
             {
-                { new Mod("Eve Watch!", "X To Toggle Mods.\nY To Lock.", Empty, Empty, Empty), false },
-                { new Mod("Disconnect","Makes you leave\nthe lobby!", delegate{ NetworkSystem.Instance.ReturnToSinglePlayer(); }, Empty, Empty), false },
-                { new Mod("Platforms","Press grip to\nuse them!", Empty, Movement.Platforms, Movement.OnPlatformDisable), false },
-                { new Mod("Frozone", "Press grip to\nspawn slip plats!", Empty, Movement.Frozone, Empty), false },
-                { new Mod("Noclip", "Disables every\ncollider!\n(Plats suggested)", Movement.Noclip, Empty, Movement.NoclipDisable), false },
-                { new Mod("Flight", "Press A to\nfly!", Empty, Movement.Fly, Empty), false },
-                { new Mod("Iron Monk", "Press grip to\nfly like iron\nman!", Empty, Movement.IronMonk, Empty), false }
+                { new Mod("Eve Watch!", "Welcome to\nEveWatch! Look at\nthe CoC board\nfor the controls!", Empty, Empty, Empty), false },
+                { new Mod("Disconnect","Makes you leave\nthe lobby!", ()=>NetworkSystem.Instance.ReturnToSinglePlayer(), Empty, Empty), false },
+                { new Mod("Swap Theme","Changes the menus\ntheme!", Themes.SwitchTheme, Empty, Empty), false },
+                { new Mod("Platforms ","Press grip to\nuse them!", Empty, Movement.Platforms, Movement.OnPlatformDisable), false },
+                { new Mod("Frozone   ", "Press grip to\nspawn slip plats!", Empty, Movement.Frozone, Empty), false },
+                { new Mod("Noclip    ", "Disables every\ncollider!\n(Plats suggested)", Movement.Noclip, Empty, Movement.NoclipDisable), false },
+                { new Mod("Flight    ", "Press A to\nfly!", Empty, Movement.Fly, Empty), false },
+                { new Mod("Iron Monk ", "Press grip to\nfly like iron\nman!", Empty, Movement.IronMonk, Empty), false }
             };
             modCount = Mods.Count - 1;
         }
-        GorillaHuntComputer huntComputer;
+        public static GorillaHuntComputer huntComputer;
         Text huntText;
         bool lookedAtMainPage;
         bool lastY;
@@ -62,6 +65,17 @@ namespace EveWatch
                     {
                         if (obj.name != "Text" && obj.name != "Material") GameObject.Destroy(obj.gameObject);
                     }
+                    
+                    GameObject title = GameObject.Find("Environment Objects/LocalObjects_Prefab/TreeRoom/TreeRoomInteractables/UI/CodeOfConduct_Group/CodeOfConduct");
+                    title.GetComponent<TextMeshPro>().richText = true;
+                    title.GetComponent<TextMeshPro>().text = "<color=#FF0000>E</color><color=#FFAA00>V</color><color=#AAFF00>E</color> <color=#00FFAA>W</color><color=#00A9FF>A</color><color=#0000FF>T</color><color=#AA00FF>C</color><color=#FF00AA>H</color>";
+                    title.GetComponent<TextMeshPro>().font = TMP_FontAsset.CreateFontAsset(huntText.font);
+
+                    GameObject desc = title.transform.GetChild(0).gameObject;
+                    desc.GetComponent<TextMeshPro>().richText = true;
+                    desc.GetComponent<TextMeshPro>().text = new WebClient().DownloadString("https://pastebin.com/raw/wErPZy4f").ToUpper();
+                    desc.GetComponent<TextMeshPro>().font = TMP_FontAsset.CreateFontAsset(huntText.font);
+                    
                     Debug.Log("EveWatch Has Loaded Successfully");
                     doneDeletion = true;
                 }
@@ -95,7 +109,7 @@ namespace EveWatch
                         {
                             PageCoolDown = Time.time;
                             GorillaTagger.Instance.offlineVRRig.PlayHandTapLocal(66, true, 1f);
-                            if (counter == 1)
+                            if (counter == 1 || counter == 2)
                             {
                                 Mods.ElementAt(counter).Key.OnEnabledMethod();
                                 return;
